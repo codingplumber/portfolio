747f95ab7ae176a23fe77bc6b00009b31584ceed
codingplumber <jason.stickel@live.com>
2017-06-27 16:54:23 -0400

    update

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+port: 3001,
+MASSIVE_URI: 'postgres://postgres:name@localhost/ecom',
+SESSION_SECRET: 'fjksdjfkl;safjksfjsal;4534jfskdlfjsdkfreyt5jsdkfjdsklfjksdfjksldfjer'
+// secretKey: 
+};
